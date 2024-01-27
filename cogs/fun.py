2275e521348df031ec10f080ef12b28c7a4d060c
Jeremy Lehmann <jeremy.lehmann06@icloud.com>
2024-01-26 21:39:41 -0800

    Added a random short story feature

--- cogs/fun.py
+++ cogs/fun.py
@@ -6,23 +6,6 @@ class FunCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="joke")
-    async def joke(self, ctx):
-        response = requests.get("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit")
-
-        if response.status_code == 200:
-            data = response.json()
-
-            if "setup" in data:
-                joke_setup = data["setup"]
-                if "delivery" in data:
-                    joke_delivery = data["delivery"]
-
-                    await ctx.send(f"{joke_setup} {joke_delivery}")
-            
-            else:
-                ctx.send("Sorry, try again later.")
-
     @commands.command(name="meme")
     async def meme(self, ctx):
         # Send a request to the meme API
@@ -46,6 +29,42 @@ class FunCommands(commands.Cog):
         else:
             # Handle the case where the request was not successful
             await ctx.send("Sorry, I couldn't fetch a meme at the moment. Try again later.")
+
+    @commands.command(name="story")
+    async def story(self, ctx):
+        response = requests.get("https://shortstories-api.onrender.com")
+
+        if response.status_code == 200:
+            data = response.json()
+
+            if "story" in data:
+                story = data["story"]
+                await ctx.send(story)
+                
+                if "moral" in data:
+                    moral = data["moral"]
+                    await ctx.send("Moral of the story:")
+                    await ctx.send(moral)
+            else:
+                await ctx.send("Sorry, try again later")
+        else:
+            await ctx.send("Sorry, try again later")
+
+    @commands.command(name="joke")
+    async def joke(self, ctx):
+        response = requests.get("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit")
+
+        if response.status_code == 200:
+            data = response.json()
+
+            if "setup" in data:
+                joke_setup = data["setup"]
+                await ctx.send(f"{joke_setup}")
+                if "delivery" in data:
+                    joke_delivery = data["delivery"]
+                    await ctx.send(f"{joke_delivery}")
+            else:
+                ctx.send("Sorry, try again later.")
 
 async def setup(bot):
     await bot.add_cog(FunCommands(bot))
